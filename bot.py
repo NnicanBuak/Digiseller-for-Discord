d289bf1e62040f47a63d5695a89ef02d28e1a067
NnicanBuak <mybailengdb@gmail.com>
2023-05-09 15:31:59 +0800

    init

--- bot.py
+++ bot.py
@@ -1,4 +1,60 @@
-# discord.py бот который является панелью для управления товарами digiseller и созданя панели магазина digiseller для дискорда
+# discord.py приложение которое создаёт интерфейс для взаимодействия с API digiseller и созданя панели магазина digiseller для дискорда, используется Digiseller API
 # Версия: 1.0
 # Автор: NnicanBuak#0001
-# Гитхаб:
+# Гитхаб: https://github.com/NnicanBuak/Digiseller-for-Discord
+import discord as ds
+from discord.app_commands import Command
+import requests
+from time import time
+from hashlib import sha256
+
+
+endpoints = {"APIlogin": "https://api.digiseller.ru/api/apilogin"}
+PUBLIC_KEY = "ae08ff4fab2b4cb2ad0da0019905a73de51a866014d34cd6f1d72566612c7cda"
+
+# Инициализация приложния
+client = ds.Client(intents=ds.Intents.all())
+guild = ds.Guild
+tree = ds.app_commands.CommandTree(
+    client=client,
+)
+
+
+# Определение всех слэш-команд (команды используют third-party API Digiseller)
+
+
+# команда авторизации /auth <SELLER_ID> <API_KEY> (для администратора)
+@tree.add_command( 
+    guild=[guild.id],
+)
+async def auth(ctx, SELLER_ID, API_KEY: str):
+    # GET запрос к API Digiseller
+    response = await requests.get(
+        endpoints["APIlogin"],
+        params={
+            "seller_id": SELLER_ID,
+            "timestamp": "1",
+            "sign": sha256(API_KEY + str(time())),
+        },
+    )
+    # Проверка на успешность запроса
+    if response.status_code != 200:
+        await ctx.respond("Ошибка, попробуйте позже")
+        return
+    # Проверка на успешность авторизации
+    if response.json()["IsSuccess"] != True:
+        await ctx.respond("Ошибка авторизации, проверьте ключ")
+        return
+    # Ответ бота
+    ctx.respond("Вы авторизовались в системе Digiseller")
+
+
+# Синхронизация команд когда клиент готов
+@client.event
+async def on_ready():
+    print("Bot is ready")
+    await tree.sync_all_commands()
+
+
+# Запуск бота
+client.run(PUBLIC_KEY)
